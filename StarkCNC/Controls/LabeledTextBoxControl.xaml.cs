ecd9cf17138ebe08bfdeb1464722dc562ccd93a6
BogachewMV <mmaxbossee@gmail.com>
2025-07-14 13:24:05 +0400

    Добавил контрол текстбокса с текстом

---
+++ StarkCNC/Controls/LabeledTextBoxControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StarkCNC.Controls
+{
+    /// <summary>
+    /// Interaction logic for LabeledTextBoxControl.xaml
+    /// </summary>
+    public partial class LabeledTextBoxControl : UserControl
+    {
+        public static readonly DependencyProperty LabelTextProperty = DependencyProperty.Register("LabelText", typeof(object), typeof(LabeledTextBoxControl), new PropertyMetadata());
+        public static readonly DependencyProperty TextBoxTextProperty = DependencyProperty.Register("TextBoxText", typeof(string), typeof(LabeledTextBoxControl), new PropertyMetadata());
+        public static readonly DependencyProperty IsReadOnlyProperty = DependencyProperty.Register("IsReadOnly", typeof(bool), typeof(LabeledTextBoxControl), new PropertyMetadata());
+
+        public object? LabelText
+        {
+            get => (object?)GetValue(LabelTextProperty);
+            set => SetValue(LabelTextProperty, value);
+        }
+
+        public string TextBoxText
+        {
+            get => (string)GetValue(TextBoxTextProperty);
+            set => SetValue(TextBoxTextProperty, value);
+        }
+
+        public bool IsReadOnly
+        {
+            get => (bool)GetValue(IsReadOnlyProperty);
+            set => SetValue(IsReadOnlyProperty, value);
+        }
+
+        public LabeledTextBoxControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
