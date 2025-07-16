525c0c622a6111ed9756ce1bd737547439f5a7e4
BogachewMV <mmaxbossee@gmail.com>
2025-07-16 14:33:18 +0400

    Создал проект и сервис, в который будет вынесена работа с запросами на контроллер станка

---
+++ StarkCNC.MachineCommunication/Services/IManualConfigurationService.cs
@@ -0,0 +1,7 @@
+﻿namespace StarkCNC.MachineCommunication.Services
+{
+    public interface IManualConfigurationService
+    {
+        public Task ConnectAsync();
+    }
+}
---
+++ StarkCNC.MachineCommunication/Services/ManualConfigurationService.cs
@@ -0,0 +1,24 @@
+﻿using OpcUaHelper;
+
+namespace StarkCNC.MachineCommunication.Services
+{
+    public class ManualConfigurationService : IManualConfigurationService
+    {
+        private readonly string _ipAddress;
+        private readonly string _configurationString;
+        private readonly OpcUaClient _client;
+
+        public ManualConfigurationService(string configurationString, string ipAddress)
+        {
+            _configurationString = configurationString;
+            _ipAddress = ipAddress;
+
+            _client = new OpcUaClient();
+        }
+
+        public async Task ConnectAsync()
+        {
+            await _client.ConnectServer(_ipAddress);
+        }
+    }
+}
