8bc7df8b18e4e279416e855014b545c2d977be26
QVelix <mmaxbossee@gmail.com>
2025-07-21 18:22:45 +0400

    Добавил View для отображения в программе

---
+++ StarkCNC.3DViewer/Views/ProgramControlView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace StarkCNC._3DViewer.Views
+{
+    /// <summary>
+    /// Interaction logic for ProgramControlView.xaml
+    /// </summary>
+    public partial class ProgramControlView : UserControl
+    {
+        public ProgramControlView()
+        {
+            InitializeComponent();
+        }
+    }
+}
