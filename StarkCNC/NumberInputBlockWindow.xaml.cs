be12ea2372ed46c6c4a7a5ab4461b54bd6ab2b1a
BogachewMV <mmaxbossee@gmail.com>
2025-07-24 13:25:13 +0400

    Сделал блок ввода

---
+++ StarkCNC/NumberInputBlockWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using StarkCNC.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StarkCNC
+{
+    /// <summary>
+    /// Interaction logic for NumberInputBlockWindow.xaml
+    /// </summary>
+    public partial class NumberInputBlockWindow : Window
+    {
+        NumberInputViewModel ViewModel;
+
+        public NumberInputBlockWindow(NumberInputViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = ViewModel;
+
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is not Button button)
+                return;
+
+            var content = button.Content;
+            if (content is null)
+                return;
+
+            var number = content.ToString() ?? string.Empty;
+            ViewModel.AddNumber(number);
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ StarkCNC/ViewModels/NumberInputViewModel.cs
@@ -0,0 +1,65 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace StarkCNC.ViewModels
+{
+    public partial class NumberInputViewModel
+    {
+        private bool _needAddPoint;
+        private string _outputValue = string.Empty;
+
+        public double ResultValue { get; set; } = 0;
+
+        public string OutputValue
+        {
+            get => _outputValue;
+            set
+            {
+                _outputValue = value;
+                TryConvertToDouble();
+            }
+        }
+
+        [RelayCommand]
+        private void Clear()
+        {
+            OutputValue = string.Empty;
+            ResultValue = 0;
+        }
+
+        [RelayCommand]
+        private void ClearLastCharacter()
+        {
+            if (OutputValue.Length == 0)
+                return;
+
+            OutputValue = OutputValue.Substring(0, OutputValue.Length - 1);
+        }
+
+        [RelayCommand]
+        private void AddPoint()
+        {
+            if (_needAddPoint)
+                return;
+
+            if (OutputValue.Length == 0)
+                OutputValue = "0";
+            OutputValue += ".";
+            _needAddPoint = true;
+        }
+
+        public void AddNumber(string number)
+        {
+            var success = Int32.TryParse(number, out var result);
+            if (!success)
+                return;
+
+            OutputValue += result;
+        }
+
+        private void TryConvertToDouble()
+        {
+            Double.TryParse(OutputValue, out var result);
+            ResultValue = result;
+        }
+    }
+}
