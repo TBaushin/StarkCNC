836ae0343104d7520d3e75fef60affe49779f2cb
BogachewMV <mmaxbossee@gmail.com>
2025-08-25 14:44:21 +0400

    Вынес стилизацию в один общий контрол

---
+++ StarkCNC/Controls/BorderedContentControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StarkCNC.Controls
+{
+    /// <summary>
+    /// Interaction logic for BorderedContentControl.xaml
+    /// </summary>
+    public partial class BorderedContentControl : UserControl
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(BorderedContentControl), new PropertyMetadata());
+        public static readonly DependencyProperty BorderedContentProperty = DependencyProperty.Register(nameof(BorderedContent), typeof(object), typeof(BorderedContentControl), new PropertyMetadata());
+
+        public string? Text
+        {
+            get => (string?)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public object? BorderedContent
+        {
+            get => (object)GetValue(BorderedContentProperty);
+            set => SetValue(BorderedContentProperty, value);
+        }
+
+        public BorderedContentControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
