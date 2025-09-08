f3530ab0e9c6f2cab05cd14a2231f9997a17a7dd
BogachewMV <mmaxbossee@gmail.com>
2025-09-08 10:06:16 +0400

    Добавил страницу настройки оснасток

---
+++ StarkCNC/Views/AdjustmentSettingsView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using StarkCNC.ViewModels;
+using System.Windows.Controls;
+
+namespace StarkCNC.Views
+{
+    /// <summary>
+    /// Interaction logic for AdjustmentSettingsView.xaml
+    /// </summary>
+    public partial class AdjustmentSettingsView : Page
+    {
+        private AdjustmentViewModel ViewModel;
+
+        public AdjustmentSettingsView(AdjustmentViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = viewModel;
+
+            InitializeComponent();
+        }
+    }
+}
