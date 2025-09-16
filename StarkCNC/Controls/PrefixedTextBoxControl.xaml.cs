4c49b06469c0e8c6ff85450f67b740a9f7caf848
BogachewMV <mmaxbossee@gmail.com>
2025-09-16 12:07:02 +0400

    Добавил контрол с подписанным полем ввода

---
+++ StarkCNC/Controls/PrefixedTextBoxControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StarkCNC.Controls
+{
+    /// <summary>
+    /// Interaction logic for PrefixedTextBoxControl.xaml
+    /// </summary>
+    public partial class PrefixedTextBoxControl : UserControl
+    {
+        public static readonly DependencyProperty PrefixProperty = DependencyProperty
+            .Register(nameof(Prefix), typeof(string), typeof(PrefixedTextBoxControl), new PropertyMetadata(""));
+        public static readonly DependencyProperty TextProperty = DependencyProperty
+            .Register(nameof(Text), typeof(string), typeof(PrefixedTextBoxControl), new PropertyMetadata(""));
+
+        public string Prefix
+        {
+            get => (string)GetValue(PrefixProperty);
+            set => SetValue(PrefixProperty, value);
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public PrefixedTextBoxControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
