8c47a38e2cf92baada0790c0c9e87c4146cc655e
BogachewMV <mmaxbossee@gmail.com>
2025-09-16 12:24:31 +0400

    Поправил отступы

--- StarkCNC/Controls/PrefixedTextBoxControl.xaml.cs
+++ StarkCNC/Controls/PrefixedTextBoxControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using StarkCNC.Helpers;
+using StarkCNC.ViewModels;
+using System.Globalization;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace StarkCNC.Controls
@@ -12,6 +15,10 @@ namespace StarkCNC.Controls
             .Register(nameof(Prefix), typeof(string), typeof(PrefixedTextBoxControl), new PropertyMetadata(""));
         public static readonly DependencyProperty TextProperty = DependencyProperty
             .Register(nameof(Text), typeof(string), typeof(PrefixedTextBoxControl), new PropertyMetadata(""));
+        public static readonly DependencyProperty NeedCallNumberInputProperty = DependencyProperty
+            .Register(nameof(NeedCallNumberInput), typeof(bool), typeof(PrefixedTextBoxControl), new PropertyMetadata());
+        public static readonly DependencyProperty IsNumericOnlyProperty = DependencyProperty
+            .Register(nameof(IsNumericOnly), typeof(bool), typeof(PrefixedTextBoxControl), new PropertyMetadata(false));
 
         public string Prefix
         {
@@ -24,10 +31,36 @@ namespace StarkCNC.Controls
             get => (string)GetValue(TextProperty);
             set => SetValue(TextProperty, value);
         }
+        public bool NeedCallNumberInput
+        {
+            get => (bool)GetValue(NeedCallNumberInputProperty);
+            set => SetValue(NeedCallNumberInputProperty, value);
+        }
+
+        public bool IsNumericOnly
+        {
+            get => (bool)GetValue(IsNumericOnlyProperty);
+            set => SetValue(IsNumericOnlyProperty, value);
+        }
 
         public PrefixedTextBoxControl()
         {
             InitializeComponent();
+        }
+
+        private void InputTextBox_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (NeedCallNumberInput)
+            {
+                var value = NumberInputViewModel.ShowDialog();
+                Text = value.ToString(CultureInfo.CurrentCulture);
+            }
+        }
+
+        private void InputTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            if (IsNumericOnly)
+                e.Handled = !OnlyNumberEnterHelper.IsTextAllowed(e.Text);
         }
     }
 }
