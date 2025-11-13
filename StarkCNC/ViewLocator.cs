d496f77e1fde769db5683214b4f3bc28ca2c836a
BogachewMV <mmaxbossee@gmail.com>
2025-11-13 16:43:54 +0400

    Добавил ViewLocator

---
+++ StarkCNC/ViewLocator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
+
+namespace StarkCNC;
+
+public class ViewLocator
+{
+    private static IServiceProvider _serviceProvider;
+
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public static Page? Build(Type param)
+    {
+        if (param is null)
+            return null;
+
+        var name = param.FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
+        var type = Type.GetType(name);
+
+        if (type is null)
+            return GenerateNotFoundPage(name);
+
+        var page = (Page)ActivatorUtilities.CreateInstance(_serviceProvider, type)!;
+        page.DataContext = ActivatorUtilities.CreateInstance(_serviceProvider, param)!;
+
+        return page;
+    }
+
+    public static Page GenerateNotFoundPage(string name) =>
+        new Page
+        {
+            Content = new Label() { Content = $"Not Found: {name}" }
+        };
+}
