33b98f102830d1b9399599df41f1e5bb81b4ebec
BogachewMV <mmaxbossee@gmail.com>
2025-11-21 10:02:50 +0400

    Удалил не нужный using

--- StarkCNC/ViewModels/MainWindowViewModel.cs
+++ StarkCNC/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Microsoft.Extensions.DependencyInjection;
 using StarkCNC.Core.Services;
 using StarkCNC.Models;
 using StarkCNC.Services;
