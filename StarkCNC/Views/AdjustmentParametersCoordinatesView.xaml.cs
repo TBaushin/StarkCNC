86dbfafc48880ba9f81b5b391646a8d79138ae58
BogachewMV <mmaxbossee@gmail.com>
2025-12-01 10:06:17 +0400

    Поправил открытие страницы координат

---
+++ StarkCNC/Views/AdjustmentParametersCoordinatesView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using StarkCNC.ViewModels;
+using System.Windows.Controls;
+
+namespace StarkCNC.Views;
+
+/// <summary>
+/// Interaction logic for AdjustmentParametersCoordinatesView.xaml
+/// </summary>
+public partial class AdjustmentParametersCoordinatesView : Page
+{
+    private AdjustmentParametersCoordinatesViewModel ViewModel;
+
+    public AdjustmentParametersCoordinatesView(AdjustmentParametersCoordinatesViewModel viewModel)
+    {
+        ViewModel = viewModel;
+        DataContext = ViewModel;
+
+        InitializeComponent();
+
+        ViewModel.PropertyChanged += Settings_PropertyChanged;
+
+        ShowOrHideElements();
+    }
+
+    private void Settings_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(AdjustmentParametersCoordinatesViewModel.IsElectricMachine))
+            ShowOrHideElements();
+    }
+
+    private void EditButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        if (sender is not Button btn)
+            return;
+
+        var parameter = btn.Name.Replace("EditButton", "", System.StringComparison.CurrentCulture);
+
+        ViewModel.EditParametersCommand.Execute(parameter);
+    }
+
+    private void ShowOrHideElements()
+    {
+        if (ViewModel.IsElectricMachine)
+        {
+            Squeeze.Visibility = System.Windows.Visibility.Visible;
+            Clamp.Visibility = System.Windows.Visibility.Visible;
+            Press.Visibility = System.Windows.Visibility.Visible;
+            Dorn.Visibility = System.Windows.Visibility.Visible;
+            Lift.Visibility = System.Windows.Visibility.Visible;
+        }
+        else
+        {
+            Squeeze.Visibility = System.Windows.Visibility.Collapsed;
+            Clamp.Visibility = System.Windows.Visibility.Collapsed;
+            Press.Visibility = System.Windows.Visibility.Collapsed;
+            Dorn.Visibility = System.Windows.Visibility.Collapsed;
+            Lift.Visibility = System.Windows.Visibility.Collapsed;
+        }
+    }
+}
