aafe110b62fcb71910fe4cdd874595f6f780831f
BogachewMV <mmaxbossee@gmail.com>
2025-12-01 10:24:53 +0400

    Добавил отображение данных

--- StarkCNC/ViewModels/AdjustmentParametersCoordinatesViewModel.cs
+++ StarkCNC/ViewModels/AdjustmentParametersCoordinatesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using StarkCNC.Core.Models.Adjustment;
 using StarkCNC.Core.Repository;
 using StarkCNC.DTO;
 
@@ -58,12 +59,76 @@ public partial class AdjustmentParametersCoordinatesViewModel : ViewModelBase
             throw new ArgumentNullException(nameof(id));
 
         var adjustment = await _repository.FindByIdAsync(guid).ConfigureAwait(false);
-        if (adjustment is not null)
-        {
-            return;
-        }
+        if (adjustment is null)
+            throw new InvalidOperationException("Не удалось найти оснастку");
 
-        throw new InvalidOperationException("Не удалось найти оснастку");
+        Bend = new AdjustmentParametersBendVisibleDto()
+        {
+            ForwardPositionLimitation = adjustment.Bend.ForwardPositionLimitation,
+            SpeedCoefficient = adjustment.Bend.SpeedCoefficient,
+            SlowdownSpeed = adjustment.Bend.SlowdownSpeed
+        };
+
+        Clamp = new AdjustmentParametersClampVisibleDto()
+        {
+            ForwardPosition = adjustment.Clamp.ForwardPosition,
+            MiddlePosition = adjustment.Clamp.MiddlePosition,
+            BackwardPosition = adjustment.Clamp.BackwardPosition,
+            SpeedCoefficient = adjustment.Clamp.SpeedCoefficient
+        };
+
+        Console = new AdjustmentParametersConsoleVisibleDto()
+        {
+            BendPosition = adjustment.Console.BendPosition,
+            SecondFloorPosition = adjustment.Console.SecondFloorPosition,
+            ThirdFloorPosition = adjustment.Console.ThirdFloorPosition,
+            PipeRotationDepartureDistance = adjustment.Console.PipeRotationDepartureDistance,
+            SpeedCoefficient = adjustment.Console.SpeedCoefficient
+        };
+
+        Dorn = new AdjustmentParametersDornVisibleDto()
+        {
+            ForwardPosition = adjustment.Dorn.ForwardPosition,
+            MiddlePosition = adjustment.Dorn.MiddlePosition,
+            BackwardPosition = adjustment.Dorn.BackwardPosition,
+            SpeedCoefficient = adjustment.Dorn.SpeedCoefficient
+        };
+
+        Lift = new AdjustmentParametersLiftVisibleDto()
+        {
+            UpperPosition = adjustment.Lift.UpperPosition,
+            MiddlePosition = adjustment.Lift.MiddlePosition,
+            LowerPosition = adjustment.Lift.LowerPosition,
+            SpeedCoefficient = adjustment.Lift.SpeedCoefficient
+        };
+
+        Press = new AdjustmentParametersPressVisibleDto()
+        {
+            ForwardPosition = adjustment.Press.ForwardPosition,
+            MiddlePosition = adjustment.Press.MiddlePosition,
+            BackwardPosition = adjustment.Press.BackwardPosition,
+            SpeedCoefficient = adjustment.Press.SpeedCoefficient
+        };
+
+        Rotation = new AdjustmentParametersRotationVisibleDto()
+        {
+            OffsetAfterZeroSearch = adjustment.Rotation.OffsetAfterZeroSearch,
+            SpeedCoefficient = adjustment.Rotation.SpeedCoefficient
+        };
+
+        Squeeze = new AdjustmentParametersSqueezeVisibleDto()
+        {
+            FrontPositionLimitation = adjustment.Squeeze.FrontPositionLimitation,
+            SpeedCoefficient = adjustment.Squeeze.SpeedCoefficient
+        };
+
+        Supply = new AdjustmentParametersSupplyVisibleDto()
+        {
+            PressZonePosition = adjustment.Supply.PressZonePosition,
+            ForwardDangerZonePosition = adjustment.Supply.ForwardDangerZonePosition,
+            ColletJawsDepth = adjustment.Supply.ColletJawsDepth,
+            SpeedCoefficient = adjustment.Supply.SpeedCoefficient
+        };
     }
 
     private async void LoadSettings()
