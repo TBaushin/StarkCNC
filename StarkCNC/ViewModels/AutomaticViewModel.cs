1780884f31a5f98f9f11fc52c9ea510238615b86
BogachewMV <mmaxbossee@gmail.com>
2025-12-05 16:02:25 +0400

    Набросал страницу автомата

--- StarkCNC/ViewModels/AutomaticViewModel.cs
+++ StarkCNC/ViewModels/AutomaticViewModel.cs
@@ -1,20 +1,58 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Configuration;
+using StarkCNC.Core.UoW;
 using StarkCNC.MachineCommunication.Services;
+using System.Collections.ObjectModel;
 
 namespace StarkCNC.ViewModels;
 
 public partial class AutomaticViewModel : ViewModelBase
 {
+    private IConfiguration _configuration;
     private IManualConfigurationService _configurationService;
 
-    public AutomaticViewModel(IManualConfigurationService configurationService)
+    [ObservableProperty]
+    private string _programName;
+
+    [ObservableProperty]
+    private double _speed;
+
+    [ObservableProperty]
+    private double _cycleTime;
+
+    [ObservableProperty]
+    private double _pipeLength;
+
+    [ObservableProperty]
+    private double _setUpPoint;
+
+    public ObservableCollection<BendingDataViewModel> BendingDatas { get; set; } = new ObservableCollection<BendingDataViewModel>();
+
+    public AutomaticViewModel(IConfiguration configuration, IManualConfigurationService configurationService, IBendingDataUnitOfWork unitOfWork)
     {
+        _configuration = configuration;
         _configurationService = configurationService;
+
+        int i = 0;
+        foreach (var item in unitOfWork.BendingDatas)
+        {
+            BendingDatas.Add(new BendingDataViewModel(i, item));
+            i += 1;
+        }
     }
 
     [RelayCommand]
     private async Task SetAutomaticMode()
     {
-        await _configurationService.WriteAsync(true, "GVL.Auto").ConfigureAwait(false);
+        var automaticTagsSection = _configuration.GetSection("AutomaticTags");
+        var turnOnRequestString = automaticTagsSection
+            .GetSection("TurnOn")
+            .GetSection("RequestString")
+            .Get<string>() ?? string.Empty;
+
+        await _configurationService
+            .WriteAsync(true, turnOnRequestString)
+            .ConfigureAwait(false);
     }
 }
