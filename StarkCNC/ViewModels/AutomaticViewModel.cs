1486ba4f9556d1bc7977e0bce5d3cbdfbb6e4557
BogachewMV <mmaxbossee@gmail.com>
2025-12-05 13:04:25 +0400

    Добавил страницу автомата

---
+++ StarkCNC/ViewModels/AutomaticViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using StarkCNC.MachineCommunication.Services;
+
+namespace StarkCNC.ViewModels;
+
+public partial class AutomaticViewModel : ViewModelBase
+{
+    private IManualConfigurationService _configurationService;
+
+    public AutomaticViewModel(IManualConfigurationService configurationService)
+    {
+        _configurationService = configurationService;
+    }
+
+    [RelayCommand]
+    private async Task SetAutomaticMode()
+    {
+        await _configurationService.WriteAsync(true, "GVL.Auto").ConfigureAwait(false);
+    }
+}
---
+++ StarkCNC/Views/AutomaticView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using StarkCNC.ViewModels;
+using System.Windows.Controls;
+
+namespace StarkCNC.Views;
+
+/// <summary>
+/// Interaction logic for AutomaticView.xaml
+/// </summary>
+public partial class AutomaticView : Page
+{
+    private AutomaticViewModel ViewModel;
+
+    public AutomaticView(AutomaticViewModel viewModel)
+    {
+        ViewModel = viewModel;
+        DataContext = ViewModel;
+
+        InitializeComponent();
+
+        ViewModel.SetAutomaticModeCommand.Execute(null);
+    }
+}
