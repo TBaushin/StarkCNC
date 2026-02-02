797fa7e3c01265f91b9178e276cde005e13ea924
TBaushin <fenbau@mail.ru>
2026-02-02 13:01:10 +0400

    зависимость

--- StarkCNC/App.xaml.cs
+++ StarkCNC/App.xaml.cs
@@ -9,6 +9,7 @@ using StarkCNC.Core.Repository;
 using StarkCNC.Core.Services;
 using StarkCNC.Core.UoW;
 using StarkCNC.Database;
+using StarkCNC.Exceptions;
 using StarkCNC.MachineCommunication.Services;
 using StarkCNC.Repository;
 using StarkCNC.Services;
