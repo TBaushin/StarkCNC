039f680da90386adaae363a0f99044c0f78541f4
BogachewMV <mmaxbossee@gmail.com>
2026-02-12 16:01:24 +0400

    Добавил дублирующие скролл кнопки

--- StarkCNC/Views/ProgramView.xaml.cs
+++ StarkCNC/Views/ProgramView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using StarkCNC.ViewModels;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace StarkCNC.Views;
 
@@ -96,5 +98,39 @@ public partial class ProgramView : Page
     private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         ViewModel.UpdateBend();
+    }
+
+    private void dgUp_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        var scrollViewer = GetInternalScrollViewer(BendingDatasDataGrid);
+        if (scrollViewer is null)
+            return;
+
+        scrollViewer.LineUp();
+    }
+
+    private void dgDown_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        var scrollViewer = GetInternalScrollViewer(BendingDatasDataGrid);
+        if (scrollViewer is null)
+            return;
+
+        scrollViewer.LineDown();
+    }
+
+    private ScrollViewer? GetInternalScrollViewer(DependencyObject element)
+    {
+        if (element is ScrollViewer scrollViewer)
+            return scrollViewer;
+
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+        {
+            var child = VisualTreeHelper.GetChild(element, i);
+            var result = GetInternalScrollViewer(child);
+            if (result is not null)
+                return result;
+        }
+
+        return null;
     }
 }
