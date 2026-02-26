167a540f4090bdf5bd144a91ce7cde41393051c8
BogachewMV <mmaxbossee@gmail.com>
2026-02-26 15:50:31 +0400

    Поправил внешний вид, изменил работу BorderedContentControl

--- StarkCNC/Controls/BorderedContentControl.xaml.cs
+++ StarkCNC/Controls/BorderedContentControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace StarkCNC.Controls;
 
@@ -8,25 +9,46 @@ namespace StarkCNC.Controls;
 /// </summary>
 public partial class BorderedContentControl : UserControl
 {
-    public static readonly DependencyProperty TextProperty = DependencyProperty
-        .Register(nameof(Text), typeof(string), typeof(BorderedContentControl), new PropertyMetadata());
+    public static readonly DependencyProperty ChangeColorOnHoverProperty = DependencyProperty
+        .Register(nameof(ChangeColorOnHover), typeof(bool), typeof(BorderedContentControl), new PropertyMetadata());
+
     public static readonly DependencyProperty BorderedContentProperty = DependencyProperty
         .Register(nameof(BorderedContent), typeof(object), typeof(BorderedContentControl), new PropertyMetadata());
 
-    public string? Text
+    public bool ChangeColorOnHover
     {
-        get => (string?)GetValue(TextProperty);
-        set => SetValue(TextProperty, value);
+        get => (bool)GetValue(ChangeColorOnHoverProperty);
+        set => SetValue(ChangeColorOnHoverProperty, value);
     }
 
     public object? BorderedContent
     {
-        get => (object)GetValue(BorderedContentProperty);
+        get => GetValue(BorderedContentProperty);
         set => SetValue(BorderedContentProperty, value);
     }
 
     public BorderedContentControl()
     {
         InitializeComponent();
+    }
+
+    private void Border_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        var border = sender as Border;
+        if (border is null)
+            return;
+
+        if (ChangeColorOnHover)
+            border.Background = (Brush)FindResource("CardBackgroundFillColorSecondaryBrush");
+    }
+
+    private void Border_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        var border = sender as Border;
+        if (border is null)
+            return;
+
+        if (ChangeColorOnHover)
+            border.Background = (Brush)FindResource("CardBackgroundFillColorDefaultBrush");
     }
 }
