35cb1be859f46eaee23661d6fd938877df37610c
BogachewMV <mmaxbossee@gmail.com>
2026-03-04 15:51:21 +0400

    Сделал новый Control - LabelWithIndicator

---
+++ StarkCNC/Controls/LabelWithIndicatorControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StarkCNC.Controls;
+
+/// <summary>
+/// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+///
+/// Step 1a) Using this custom control in a XAML file that exists in the current project.
+/// Add this XmlNamespace attribute to the root element of the markup file where it is 
+/// to be used:
+///
+///     xmlns:MyNamespace="clr-namespace:StarkCNC.Controls"
+///
+///
+/// Step 1b) Using this custom control in a XAML file that exists in a different project.
+/// Add this XmlNamespace attribute to the root element of the markup file where it is 
+/// to be used:
+///
+///     xmlns:MyNamespace="clr-namespace:StarkCNC.Controls;assembly=StarkCNC.Controls"
+///
+/// You will also need to add a project reference from the project where the XAML file lives
+/// to this project and Rebuild to avoid compilation errors:
+///
+///     Right click on the target project in the Solution Explorer and
+///     "Add Reference"->"Projects"->[Browse to and select this project]
+///
+///
+/// Step 2)
+/// Go ahead and use your control in the XAML file.
+///
+///     <MyNamespace:LabelWithIndicatorControl/>
+///
+/// </summary>
+public class LabelWithIndicatorControl : Control
+{
+    public static readonly DependencyProperty IsActiveProperty = DependencyProperty
+        .Register(nameof(IsActive), typeof(bool), typeof(LabelWithIndicatorControl), new PropertyMetadata());
+    public static readonly DependencyProperty TextProperty = DependencyProperty
+        .Register(nameof(Text), typeof(string), typeof(LabelWithIndicatorControl), new PropertyMetadata());
+
+    public bool IsActive
+    {
+        get => (bool)GetValue(IsActiveProperty);
+        set => SetValue(IsActiveProperty, value);
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    static LabelWithIndicatorControl()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(LabelWithIndicatorControl), new FrameworkPropertyMetadata(typeof(LabelWithIndicatorControl)));
+    }
+}
