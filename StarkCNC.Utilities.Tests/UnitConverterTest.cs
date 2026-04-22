c2de68626b7122882d724d314d5dc676fc66bc8a
BogachewMV <mmaxbossee@gmail.com>
2026-04-22 16:30:40 +0400

    Добавил конвертер, чтоб ошибки не выбрасывались

---
+++ StarkCNC.Utilities.Tests/UnitConverterTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+
+namespace StarkCNC.Utilities.Tests;
+
+public class UnitConverterTest
+{
+    [Theory]
+    [InlineData(50, "шт.")]
+    [InlineData(24, "сек.")]
+    public void ConvertIntTest(int value, string unit)
+    {
+        // Arrange
+        var converter = new UnitConverter();
+        // Act
+        var result = converter.Convert(value, typeof(string), unit, CultureInfo.InvariantCulture);
+        // Assert
+        Assert.Equivalent($"{value} {unit}", result);
+    }
+
+    [Theory]
+    [InlineData(50, "шт.")]
+    [InlineData(25.7, "сек.")]
+    [InlineData(99.979, "шт.")]
+    public void ConvertFloatTest(float value, string unit)
+    {
+        // Arrange
+        var converter = new UnitConverter();
+        // Act
+        var result = converter.Convert(value, typeof(string), unit, CultureInfo.InvariantCulture);
+        // Assert
+        Assert.Equivalent($"{value} {unit}", result);
+    }
+
+    [Theory]
+    [InlineData("50 шт.", 50)]
+    [InlineData("24 сек.", 24)]
+    public void ConvertBackInt(string value, int numberExpected)
+    {
+        // Arrange
+        var converter = new UnitConverter();
+        // Act
+        var result = (int)converter.ConvertBack(value, typeof(int), null, CultureInfo.InvariantCulture);
+        // Assert
+        Assert.Equal(numberExpected, result);
+    }
+
+    [Theory]
+    [InlineData("50 шт.", 50)]
+    [InlineData("25.7 сек.", 25.7)]
+    [InlineData("99.979 шт.", 99.979)]
+    public void ConvertBackFloatTest(string value, float numberExpected)
+    {
+        // Arrange
+        var converter = new UnitConverter();
+        // Act
+        var result = (float)converter.ConvertBack(value, typeof(float), null, CultureInfo.InvariantCulture);
+        // Assert
+        Assert.Equal(numberExpected, result);
+    }
+}
---
+++ StarkCNC.Utilities/UnitConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+
+namespace StarkCNC.Utilities;
+
+public class UnitConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return $"{value} {parameter}";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string s)
+        {
+            var reg = new Regex(@"\s{0,}\D+(\.){0,1}\D+(\.){0,}");
+            string resultString = reg.Replace(s, "");
+            return targetType switch
+            {
+                var t when t == typeof(int) => int.TryParse(resultString, culture, out var result) ? result : value,
+                var t when t == typeof(float) => float.TryParse(resultString, culture, out var result) ? result : value,
+                var t when t == typeof(double) => double.TryParse(resultString, culture, out var result) ? result : value,
+                _ => value
+            };
+        }
+
+        return value;
+    }
+}
